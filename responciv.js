627f1e252aad61d80199e8f58d91c70539bb7398
Кирилл Б. <meded90@gmail.com>
2013-06-01 16:46:51 +0600

    Create responciv.js

---
+++ responciv.js
@@ -0,0 +1,23 @@
+ var isDevice, whatDevice;
+
+whatDevice = function () {
+    // console.log('whatDevic');
+    if (Modernizr.mq('(min-width: 1200px)')) {
+        return isDevice('large-desktop');
+    } else if (Modernizr.mq('(min-width: 980px) and (max-width: 1199px)')) {
+        return isDevice('desktop');
+    } else if (Modernizr.mq('(min-width: 768px) and (max-width: 979px)')) {
+        return isDevice('tablet');
+    } else if (Modernizr.mq('(min-width: 481px) and (max-width: 767px)')) {
+        return isDevice('phone');
+    } else if (Modernizr.mq('(max-width: 480px)')) {
+        return isDevice('mobile');
+    }
+};
+
+isDevice = function (name) {
+    if (window.mqStatus !== name) {
+        window.mqStatus = name;
+        $(document).trigger(name);
+    }
+};
